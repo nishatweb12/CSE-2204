429345f24456af67e9a96021eb69763e4dc2b17e
nishatweb12 <nishatanchal@gmail.com>
2025-08-19 23:44:27 +0600

    Add files via upload

---
+++ problem.py
@@ -0,0 +1,3 @@
+n = int (input ("Enter a number :"))
+sum_num = n*(n+1)//2
+print("sum of 1st",n,"natural number is:",sum_num)
---
+++ problem5.py
@@ -0,0 +1,9 @@
+a = float (input("1st:"))
+b = float (input("2nd:"))
+op = input("OP(+,-,*,/):")
+if op == "+": print (a+b)
+elif op == "-": print(a-b)
+elif op == "*": print(a*b)
+elif op == "/": print (a/b )
+else:
+    print("Invalid")
